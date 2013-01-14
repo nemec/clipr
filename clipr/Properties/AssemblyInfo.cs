d7fbe3da8138be1eee345288d91b96d397b1b732
Dan Nemec <djnemec@gmail.com>
2013-01-14 00:32:33 -0600

    Updated with NuGet information.

--- clipr/Properties/AssemblyInfo.cs
+++ clipr/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("clipr")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("clipr Command Line Interface ParseR")]
+[assembly: AssemblyDescription("Simple command line argument parser with automated help generator.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dan Nemec")]
 [assembly: AssemblyProduct("clipr")]
