8cff0127c1011aadde208ee16c0559e6ad9ebca5
Dan Nemec <dan@nem.ec>
2016-12-13 22:28:40 -0800

    Fix resources build on .Net Core

--- clipr/Properties/Resources.Designer.cs
+++ clipr/Properties/Resources.Designer.cs
@@ -10,8 +10,9 @@
 
 namespace clipr.Properties {
     using System;
-    
-    
+    using System.Reflection;
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -39,7 +40,12 @@ namespace clipr.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("clipr.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("clipr.Properties.Resources",
+#if NETCORE
+                        typeof(Resources).GetTypeInfo().Assembly);
+#else
+                        typeof(Resources).Assembly);
+#endif
                     resourceMan = temp;
                 }
                 return resourceMan;
