885234073da9e05d8c815f61bf6c22f87403ca9c
Dan Nemec <dan@nem.ec>
2017-09-25 22:06:30 -0700

    Update doc link root

--- build-docs.ps1
+++ build-docs.ps1
@@ -1,14 +1,15 @@
 param
 (
     [ValidateSet("Build","Watch")]
-    [string]$Action = "Build"
+    [string]$Action="Build"
 )
 
-if($Action = "Build")
+
+if($Action -eq "Build")
 {
     & wyam build -c "src-docs/config.wyam" -i "src-docs/Input" -o "docs/"
 }
 else
 {
-    & wyam build -c "src-docs/config.wyam" -p --watch -i "src-docs/Input" -o "docs/"
+    & wyam build -c "src-docs/config.wyam" -p --watch --virtual-dir "/clipr" -i "src-docs/Input" -o "docs/"
 }
